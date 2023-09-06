b347e576754636104830d2a2bb8b8ee40c83587a
tomrod10 <tomrod243@gmail.com>
2023-09-05 22:49:43 -0700

    serve app in express server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, "..", 'dist')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
 
 app.listen(port, () => {
