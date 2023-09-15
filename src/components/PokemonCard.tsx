639dbbf90ee52f9a77028f20bb4a028b1fe664c7
tomrod10 <tomrod243@gmail.com>
2023-09-14 19:22:40 -0700

    move button to child component and fix cls on generate

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -12,39 +12,48 @@ type PokemonCardProps = {
 
 export const PokemonCard = ({
   pokemon,
-  isLoading
+  isLoading,
+  handleGenerate
 }: PokemonCardProps) => {
+  const loaderHeight = "0.5em"
+  const loaderColor = "rgb(224 231 255"
   return (
-    <div>
-      {isLoading ? (
-        <div className="px-auto my-auto h-[555px]">
-          <BarLoader />
-        </div>
-      ) : (
-        <>
-          <div className="flex flex-col items-center">
-            <h2>{pokemon.name}</h2>
+    <div className="flex flex-col items-center h-[554px]">
+      <div>
+        {isLoading ? (
+          <div className="h-[32px] flex items-center">
+            <BarLoader color={loaderColor} height={loaderHeight}/>
           </div>
-          <img className="h-[475px]" alt="ditto-png" src={pokemon.image} />
-          <div>
-            {/* Make the stats a ul element with each stat being an li element */}
-            <ul className="flex flex-row justify-between p-4">
-              <li>
-              Type: {pokemon.type}
-              </li>
-              <li>
-              HP: {pokemon.hp}
-              </li>
-              <li>
-              Attack: {pokemon.attack}
-              </li>
-              <li>
-              Defense: {pokemon.defense}
-              </li>
-            </ul>
+        ) : (
+          <h2 className="text-2xl text-center">{pokemon.name}</h2>
+        )}
+      </div>
+      <div>
+        {isLoading ? (
+          <div className="h-[475px] flex items-center">
+            <BarLoader color={loaderColor} height={loaderHeight} width="12em"/>
           </div>
-        </>
-      )}
+        ) : (
+          <img className="h-[475px] w-full" alt={`${pokemon.name}-png`} src={pokemon.image} />
+        )}
+      </div>
+      <div className="w-[475px] h-[56px]">
+        <ul className="flex flex-row p-4 justify-between">
+          <li>
+            Type: {pokemon.type}
+          </li>
+          <li>
+            HP: {pokemon.hp}
+          </li>
+          <li>
+            Attack: {pokemon.attack}
+          </li>
+          <li>
+            Defense: {pokemon.defense}
+          </li>
+        </ul>
+      </div>
+      <button className="bg-indigo-100 hover:bg-indigo-200 text-blue-700 p-1.5 rounded-xl" onClick={handleGenerate}>Generate</button>
     </div>
   );
 };
