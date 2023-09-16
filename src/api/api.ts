e0abcfcd8c1986f8e6de23254f0bfdab96db9d19
tomrod10 <tomrod243@gmail.com>
2023-09-16 10:28:37 -0700

    improve readability

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,26 +9,26 @@ export type Pokemon = {
 
 type FetchPokemonRes = {
   response: Pokemon;
-  error?: Error | null;
+  error?: Error | null | unknown;
 };
 
 export const fetchPokemon = async (id: number): Promise<FetchPokemonRes> => {
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    if (res.ok === true) {
-      const pokemon = await res.json();
-      const pokemonData = {
-        name: pokemon.name,
-        image: pokemon?.sprites?.other?.["official-artwork"]?.front_default,
-        type: pokemon.types[0].type.name,
-        hp: pokemon.stats[0].base_stat,
-        attack: pokemon.stats[1].base_stat,
-        defense: pokemon.stats[2].base_stat
-      };
-      return { response: pokemonData, error: null };
-    }
+    if (!res.ok === true) {
+      throw new Error(`Failed fetch for Pokemon with id: ${id}`);
+    };
 
-    throw new Error(`Failed fetch for Pokemon with id: ${id}`);
+    const pokemon = await res.json();
+    const pokemonData: Pokemon = {
+      name: pokemon.name,
+      image: pokemon?.sprites?.other?.["official-artwork"]?.front_default,
+      type: pokemon.types[0].type.name,
+      hp: pokemon.stats[0].base_stat,
+      attack: pokemon.stats[1].base_stat,
+      defense: pokemon.stats[2].base_stat
+    }
+    return { response: pokemonData, error: null };
   } catch (error) {
     console.error(`${error}`);
     return {
