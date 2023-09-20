2f3834b168b8cfd39b0cce2dc47ecc230c04a693
tomrod10 <tomrod243@gmail.com>
2023-09-20 14:34:38 -0700

    changes to webpack config and run script

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
     port: 3010,
     open: true,
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
   module: {
     rules: [
       {
